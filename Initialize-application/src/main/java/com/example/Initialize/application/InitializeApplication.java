c020ad90d491a7e27034ebfa19613362dcb87357
shivapant <pant.malay10@gmail.com>
2021-12-22 12:38:19 +0530

    first commit

---
+++ Initialize-application/src/main/java/com/example/Initialize/application/InitializeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Initialize.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializeApplication.class, args);
+	}
+
+}
